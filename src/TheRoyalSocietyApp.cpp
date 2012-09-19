6748daf95cb499a5a6611a665788a01e9aadaf45
Andrew Duberstein <ajduberstein@gmail.com>
2012-09-18 22:18:17 -0400

    Shapes can be ordered
    Demonstrated using an array that the shapes can be ordered. Pretty cool.

--- src/TheRoyalSocietyApp.cpp
+++ src/TheRoyalSocietyApp.cpp
@@ -11,6 +11,8 @@
 #include "cinder/ImageIo.h"
 #include <vector>
 //#include "TheRoyalSocietyApp.h"
+#include <math.h>
+#define PI 3.14159265
 
 using namespace ci;
 using namespace ci::app;
@@ -28,9 +30,11 @@ class TheRoyalSocietyApp : public AppBasic {
 	void render();
   private:
 	 Font master_font_;
+	 int color_changer_;
 	 gl::Texture master_texture_font_;
 	 bool display_help;
 	 Vec2f master_size;
+	 int param_;
 	 const static int kInitialNumberOfShapes = 5;
 	 std::vector<int> shape_id_;
 };
@@ -68,13 +72,15 @@ void TheRoyalSocietyApp::setup(){
 		master_size = Vec2f( 100, 100 );
 		render();
 		display_help = true;
-		int id_tmp[] = {1,2,3,4,5};
+		int id_tmp[] = {3,1,2,4};
 		shape_id_ = std::vector<int>(kInitialNumberOfShapes);
-		shape_id_.assign(id_tmp,id_tmp+4);
+		shape_id_.assign(id_tmp,id_tmp+5);
+		color_changer_=0;
+		param_=0;
 }
 
 void TheRoyalSocietyApp::render(){
-	string txt = "Welcome to the window manager!\n\n Keybindings: \n j      Moves shapes down \n k      Moves shapes up \n ~     Reverses shape order \n y     Selects foremost shape \n p     Pastes shapes(s)";
+	string txt = "Welcome to the window manager!\n\n Keybindings: \n j      Moves shapes down \n k      Moves shapes up \n ~     Reverses shape order \n y     Selects foremost shape \n p     Pastes shape(s)";
 	TextBox tbox = TextBox().alignment( TextBox::CENTER ).font(master_font_).size( Vec2i( 512, 511) ).text( txt );
 	tbox.setColor( Color( 1.0f, 0.65f, 0.35f ) );
 	tbox.setBackgroundColor( ColorA( 0.5, 0, 0, 1 ) );
@@ -89,33 +95,39 @@ void TheRoyalSocietyApp::mouseDown( MouseEvent event )
 
 void TheRoyalSocietyApp::update()
 {
+	param_++;
+	color_changer_ = sin(param_*PI/1800)*100;
+	if (param_>200)
+		param_ = 0;
 }
 
 void TheRoyalSocietyApp::draw(){
 	// clears out the window with black
 	gl::clear( Color( 0, 0, 0 ) );
-	gl::color(Color8u(0,255,0));
-	gl::drawSolidCircle(Vec2f(65.0f,65.0f),50.0f,5);
-	gl::color(Color8u(240,255,0));
-	gl::drawSolidCircle(Vec2f(55.0f,55.0f),50.0f,5);
-	gl::color(Color8u(255,255,0));
-	gl::drawSolidCircle(Vec2f(255.0f,255.0f),50.0f,4);
-	for (int i = 0; i < 3; i++){
+	for (int i = 0; i < 4; i++){
 		if (shape_id_[i] == 1){
-			gl::color(Color8u(0,0,200));
-			gl::drawSolidCircle(Vec2f(300,300),200,1000);
+			gl::color(Color8u(50,0,0));
+			gl::drawSolidCircle(Vec2f(310-color_changer_,310+color_changer_),200,100);
+			gl::color(Color8u(410,0,0));
+			gl::drawSolidCircle(Vec2f(300-color_changer_,300+color_changer_),200,100);
 		}
 		else if (shape_id_[i] == 2){
-			gl::color(Color8u(0,0,200));
-			gl::drawSolidCircle(Vec2f(400,400),200,1100);
+			gl::color(Color8u(0,0,50));
+			gl::drawSolidCircle(Vec2f(410+color_changer_/2,410),200,10);
+			gl::color(Color8u(0,0,410));
+			gl::drawSolidCircle(Vec2f(400+color_changer_/2,400),200,10);
 		}
 		else if (shape_id_[i] == 3){
-			gl::color(Color8u(0,0,200));
-			gl::drawSolidCircle(Vec2f(500,500),200,1200);
+			gl::color(Color8u(0,50,0));
+			gl::drawSolidCircle(Vec2f(510,310+color_changer_/5),200/(1+abs(tan((double) param_)),4));
+			gl::color(Color8u(0,410,0));
+			gl::drawSolidCircle(Vec2f(500,300+color_changer_/5),200/(1+abs(tan((double) param_))),4);
 		}
 		else if (shape_id_[i] == 4){
-			gl::color(Color8u(0,0,200));
-			gl::drawSolidCircle(Vec2f(300,300),200,1000);
+			gl::color(Color8u(0,100,100));
+			gl::drawSolidCircle(Vec2f(310,110),color_changer_*2.5,3);
+			gl::color(Color8u(0,300,300));
+			gl::drawSolidCircle(Vec2f(300,100),color_changer_*2.6,3*rand()%2);
 		}
 		else{
 			break;
